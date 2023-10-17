914863398c9da3856c4a3c216afcfab5222910be
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2023-10-17 18:57:44 +0800

    Added a file

--- non_printable.c
+++ non_printable.c
@@ -9,36 +9,36 @@
  */
 int non_print(char *str)
 {
-    int count;
+	int count;
 
-    count = 0;
-    if (!str)
-    {
-        print_str("(null)");
-    }
-    while (*str)
-    {
-        if ((*str > 0 && *str < 32) || *str >= 127)
-        {
-            count += print_str("\\x");
-            if (*str <= 0x0f)
-            {
-                print_char('0');
-                count += print_digit(*str, 16, 1);
-                ++str;
-            }
-            else
-            {
-                count += print_digit(*str, 16, 1);
-                ++str;
-            }
-        }
-        else 
-        {
-            print_char((int)*str);
-            ++str;
-            ++count;
-        }
-    }
-    return (count);
+	count = 0;
+	if (!str)
+	{
+		print_str("(null)");
+	}
+	while (*str)
+	{
+		if ((*str > 0 && *str < 32) || *str >= 127)
+		{
+			count += print_str("\\x");
+			if (*str <= 0x0f)
+			{
+				print_char('0');
+				count += print_digit(*str, 16, 1);
+				++str;
+			}
+			else
+			{
+				count += print_digit(*str, 16, 1);
+				++str;
+			}
+		}
+		else
+		{
+			print_char((int)*str);
+			++str;
+			++count;
+		}
+	}
+	return (count);
 }
